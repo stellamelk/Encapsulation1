d808fabec2499f1f21ecadfefa3f45245cc6f360
Stella Melkonyan <stella.melkonyan.benivo@outlook.com>
2021-11-11 09:30:14 +0400

    Add project files.

---
+++ Encapsulation1/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation1
+{
+    class Bank
+    {
+        //hiding class data by declaring the variable as private
+        private decimal _balance;
+        //create public setter and getter
+        public decimal getBalance()
+        {
+            return _balance;
+        }
+        public void setBalance(decimal balance)
+        {
+            _balance = balance; 
+        }
+    }
+}
---
+++ Encapsulation1/BankUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation1
+{
+    class BankUser
+    {
+        static void Main(string[] args)
+        {
+            Bank VTB = new Bank();
+            VTB.setBalance(500);
+            Console.WriteLine($"Set money balance is {VTB.getBalance()} USD");
+            Console.WriteLine("Press any key to exist");
+            Console.ReadLine();
+        }
+    }
+}
